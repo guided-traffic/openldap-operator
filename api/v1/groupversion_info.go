e55018942a96f62172f9e844276acfe9589469d3
Hans Fischer <mail@hans-fischer.com>
2026-05-06 06:53:55 +0200

    fix: linting

--- api/v1/groupversion_info.go
+++ api/v1/groupversion_info.go
@@ -20,8 +20,9 @@ limitations under the License.
 package v1
 
 import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	"sigs.k8s.io/controller-runtime/pkg/scheme"
 )
 
 var (
@@ -29,8 +30,25 @@ var (
 	GroupVersion = schema.GroupVersion{Group: "openldap.guided-traffic.com", Version: "v1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
-	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
+	SchemeBuilder = &Builder{GroupVersion: GroupVersion}
 
 	// AddToScheme adds the types in this group-version to the given scheme.
 	AddToScheme = SchemeBuilder.AddToScheme
 )
+
+// Builder registers Kubernetes API types for a GroupVersion without depending
+// on controller-runtime, whose scheme.Builder is deprecated for use in api packages.
+type Builder struct {
+	GroupVersion schema.GroupVersion
+	runtime.SchemeBuilder
+}
+
+// Register adds the given types to the SchemeBuilder under the configured GroupVersion.
+func (b *Builder) Register(types ...runtime.Object) *Builder {
+	b.SchemeBuilder.Register(func(scheme *runtime.Scheme) error {
+		scheme.AddKnownTypes(b.GroupVersion, types...)
+		metav1.AddToGroupVersion(scheme, b.GroupVersion)
+		return nil
+	})
+	return b
+}
